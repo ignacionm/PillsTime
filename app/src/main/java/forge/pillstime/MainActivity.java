2ed39d1cf3f2543ad0211c0ad71c3387af9aa7b8
ignacionm <ignacio.nm28@gmail.com>
2015-10-15 12:36:48 -0700

    Initial commit

---
+++ app/src/main/java/forge/pillstime/MainActivity.java
@@ -0,0 +1,75 @@
+package forge.pillstime;
+
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity  implements ActionBar.TabListener , ViewPager.OnPageChangeListener {
+    private ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        PagerAdapter adapter = new PagerAdapter(getSupportFragmentManager());
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(adapter);
+
+        mViewPager.setOnPageChangeListener(this);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        ActionBar.Tab tab = actionBar.newTab().setText("Alarmas").setTabListener(this);
+        actionBar.addTab(tab);
+
+        tab = actionBar.newTab().setText("Historial").setTabListener(this);
+        actionBar.addTab(tab);
+
+        tab = actionBar.newTab().setText("Consejos").setTabListener(this);
+        actionBar.addTab(tab);
+
+        tab = actionBar.newTab().setText("Compras").setTabListener(this);
+        actionBar.addTab(tab);
+    }
+
+    //implements on pager selected
+    @Override
+    public void onPageScrolled(int i, float v, int i2) {
+
+    }
+
+    @Override
+    public void onPageSelected(int i) {
+        getSupportActionBar().setSelectedNavigationItem(i);
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int i) {
+
+    }
+
+
+    //implements tab listener
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+        mViewPager.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+
+    }
+
+
+}
